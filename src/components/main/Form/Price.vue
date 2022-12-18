ebcd5593c5b38130a48eb50a0900cdfdf388711f
Dmitriev Denis <89563336+SbokyZahodi@users.noreply.github.com>
2022-12-18 17:39:45 +1000

    double range

--- src/components/main/Form/Price.vue
+++ src/components/main/Form/Price.vue
@@ -6,12 +6,69 @@
     <div
       class="bg-slate-100 p-2 flex w-72 text-sm rounded-md justify-between font-semibold"
     >
-      <span>от 3 млн ₽</span>
-      <span>до 7 млн ₽</span>
+      <span>от {{ from }} млн ₽</span>
+      <span>до {{ to }} млн ₽</span>
+    </div>
+    <div class="flex">
+      <input
+        type="range"
+        name=""
+        id=""
+        class="w-[140px]"
+        min="3"
+        max="5"
+        v-model="from"
+      />
+      <input
+        type="range"
+        name=""
+        id=""
+        class="w-[140px]"
+        min="5"
+        max="7"
+        v-model="to"
+      />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      from: 3,
+      to: 7,
+    };
+  },
+};
 </script>
+
+<style scoped>
+input[type="range"] {
+  -webkit-appearance: none;
+  height: 2px;
+  background: red;
+  background-repeat: no-repeat;
+}
+
+input[type="range"]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  border-color: red;
+  border: 1px solid red;
+  background: white;
+  cursor: ew-resize;
+  box-shadow: 0 0 2px 0 #555;
+  transition: background 0.3s ease-in-out;
+}
+
+input[type="range"]::-webkit-slider-runnable-track {
+  -webkit-appearance: none;
+  box-shadow: none;
+  border-radius: 1px;
+  border: none;
+  background: transparent;
+}
+</style>
